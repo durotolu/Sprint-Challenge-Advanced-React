6fe24663260cf76f50b2fd7e1a5cd7ca3a97597b
Modurotolu Olokode <durolawk@gmail.com>
2019-10-04 08:40:35 +0100

    created class component fetching data

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,35 @@
 import React from 'react';
-import logo from './logo.svg';
+import axios from 'axios';
 import './App.css';
 
-function App() {
-  return (
+const playersApi = 'http://localhost:5000/api/players'
+
+class App extends React.Component {
+  constructor() {
+    super();
+      this.state = {
+        playersInfo: []
+      }
+  }
+
+  componentDidMount() {
+    axios.get(playersApi)
+    .then((res) => {
+      this.setState({playersInfo: res.data})
+    })
+    .catch((err) => {
+      debugger
+    })
+  }
+
+  render () {
+    return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
     </div>
   );
+  }
+  
 }
 
 export default App;
